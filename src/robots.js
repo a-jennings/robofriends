3ced59fe657bfdb12c7ddffeaf589a27727a44ce
Andrew Jennings <andrew_jennings@ymail.com>
2020-04-10 11:48:05 +0100

    added more cards, added function CardList

---
+++ src/robots.js
@@ -0,0 +1,66 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John Smith',
+        username: 'Jsmith',
+        email: 'Jsmith@golden.com'
+    },
+    {
+        id: 2,
+        name: 'April Ludgate',
+        username: 'Diablo1',
+        email: 'makeyouscream@death.com'
+    },
+    {
+        id: 3,
+        name: 'Jon Jones',
+        username: '12time',
+        email: 'partytime@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Andrew Jennings',
+        username: 'pur3Vengeance',
+        email: 'a-jennings123@foo.com'
+    },
+    {
+        id: 5,
+        name: 'Matt Mercer',
+        username: 'MM',
+        email: 'm.mercer@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Cloud Strife',
+        username: 'troubles',
+        email: 'cloud@midgar.com'
+    },
+    {
+        id: 7,
+        name: 'Dungeon Master',
+        username: 'GodDM',
+        email: 'pain@lych.com'
+    },
+    {
+        id: 8,
+        name: 'Strahd Von Zarovich',
+        username: 'vamp666',
+        email: 'strahdtheimpaler@gmail.org'
+    },
+    {
+        id: 9,
+        name: 'Peter Collington',
+        username: 'iloveR6',
+        email: 'peterbitch@ymail.com'
+    },
+    {
+        id: 10,
+        name: 'Scanlan Gnome',
+        username: 'moneymoney',
+        email: 'poosay@slayer.com'
+    },
+
+
+
+
+]
